2b138e5d212237bb2bf3444b32dd5e4abc4e2ab8
vedang desai <desaivedang7@gmail.com>
2021-03-13 01:46:11 +0530

    add angular flex layout, add business details component.

--- booking-management/src/app/app.module.ts
+++ booking-management/src/app/app.module.ts
@@ -7,13 +7,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { MainComponent } from './main/main.component';
 import {MatTabsModule} from '@angular/material/tabs';
+import { BusinessDetailsComponent } from './components/business-details/business-details.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    BusinessDetailsComponent,
   ],
   imports: [
     BrowserModule,
@@ -21,8 +30,16 @@ import {MatTabsModule} from '@angular/material/tabs';
     BrowserAnimationsModule,
     MatSidenavModule,
     MatTabsModule,
+    MatCheckboxModule,
+    FlexLayoutModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports:[
+   
+  ]
 })
 export class AppModule { }
