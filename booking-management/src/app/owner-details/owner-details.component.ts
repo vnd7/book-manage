f1f3b56674176d0fcd092c8f3a93da2b8cd811d2
vedang desai <desaivedang7@gmail.com>
2021-03-13 17:27:29 +0530

    solved bugs.

--- booking-management/src/app/owner-details/owner-details.component.ts
+++ booking-management/src/app/owner-details/owner-details.component.ts
@@ -7,9 +7,116 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OwnerDetailsComponent implements OnInit {
 
+  f1:any;
+  b1:any;
+  f2:any;
+  b2:any;
+  f3:any;
+  b3:any;
+  f4:any;
+  b4:any;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  
+  readImageF1(event:any){
+    console.log('event in readimage',event);
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+
+      const reader = new FileReader();
+      reader.onload = e => this.f1 = reader.result;
+
+      reader.readAsDataURL(file);
+  }
+  }
+
+  readImageB1(event:any){
+    console.log('event in readimage',event);
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+
+      const reader = new FileReader();
+      reader.onload = e => this.b1 = reader.result;
+
+      reader.readAsDataURL(file);
+  }
+  }
+
+  readImageF2(event:any){
+    console.log('event in readimage',event);
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+
+      const reader = new FileReader();
+      reader.onload = e => this.f2 = reader.result;
+
+      reader.readAsDataURL(file);
+  }
+  }
+
+  readImageB2(event:any){
+    console.log('event in readimage',event);
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+
+      const reader = new FileReader();
+      reader.onload = e => this.b2 = reader.result;
+
+      reader.readAsDataURL(file);
+  }
+  }
+
+  readImageF3(event:any){
+    console.log('event in readimage',event);
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+
+      const reader = new FileReader();
+      reader.onload = e => this.f3 = reader.result;
+
+      reader.readAsDataURL(file);
+  }
+  }
+
+  readImageB3(event:any){
+    console.log('event in readimage',event);
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+
+      const reader = new FileReader();
+      reader.onload = e => this.b3 = reader.result;
+
+      reader.readAsDataURL(file);
+  }
+  }
+
+  readImageF4(event:any){
+    console.log('event in readimage',event);
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+
+      const reader = new FileReader();
+      reader.onload = e => this.f4 = reader.result;
+
+      reader.readAsDataURL(file);
+  }
+  }
+
+  readImageB4(event:any){
+    console.log('event in readimage',event);
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+
+      const reader = new FileReader();
+      reader.onload = e => this.b4 = reader.result;
+
+      reader.readAsDataURL(file);
+  }
+  }
+
 
 }
